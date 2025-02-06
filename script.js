a6e0440827069dba23152444e93e966f78153623
WEINBOI <weingaertner.elias@gmail.com>
2025-02-06 16:43:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Hier könnten interaktive JavaScript-Elemente platziert werden, z.B. für die Navigation oder ein Kontaktformular
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website geladen!');
+});
